03eaaf9371b3a370883d2958563474508aaa04a7
JW-Ahn0 <ajwquestion@gmail.com>
2022-08-12 02:27:16 +0900

    .gitignore apply

--- build/resources/main/data.sql
+++
@@ -1,6 +0,0 @@
-ALTER table item convert to charset utf8;
-ALTER table champion convert to charset utf8;
-ALTER table item_full convert to charset utf8;
-ALTER table perk_id convert to charset utf8;
-ALTER table solo convert to charset utf8;
-ALTER table spell convert to charset utf8;
