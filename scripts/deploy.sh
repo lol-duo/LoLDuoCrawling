1b84f05c25cc668bdc3cd70702eb6843e1a937fc
JW-Ahn0 <ajwquestion@gmail.com>
2022-08-16 15:58:05 +0900

    fix/tier Map 관련해서 null 나오는 문제 fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,4 +19,4 @@ echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-nohup /opt/jdk-17/bin/java -javaagent:$DATADOG -Ddd.logs.injection=true -jar -Dspring.profiles.active=server $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup /opt/jdk-17/bin/java -javaagent:$DATADOG -Ddd.logs.injection=true -jar -Dspring.profiles.active=devserverdb $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
