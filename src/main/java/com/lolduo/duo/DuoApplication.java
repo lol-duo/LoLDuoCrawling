19a568b7e49e92efb15bcf540f71d1bc66a6c5ff
JW-Ahn0 <jun.woo.an0@gmail.com>
2022-11-05 20:10:22 +0900

    PLATINUM tier 주석처리

--- src/main/java/com/lolduo/duo/DuoApplication.java
+++ src/main/java/com/lolduo/duo/DuoApplication.java
@@ -1,12 +1,12 @@
 package com.lolduo.duo;
 
+import com.lolduo.duo.service.RiotService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.metrics.export.datadog.EnableDatadogMetrics;
 
 @SpringBootApplication
 public class DuoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DuoApplication.class, args);
     }
